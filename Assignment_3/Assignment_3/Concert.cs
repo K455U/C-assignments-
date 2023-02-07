67910c4b881fe5768e12700ccc4d28a0be1037d2
Kasperi Mannersalo <117868186+K455U@users.noreply.github.com>
2023-02-08 00:29:11 +0200

    Add files via upload

---
+++ Assignment_3/Assignment_3/Concert.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    internal class Concert
+    {
+        string Title { get; set; }
+        string Location { get; set; }
+        string Date { get; set; }
+        string Time { get; set; }
+        public double Price { get; set; }
+
+        // Default constructor
+        public Concert()
+        {
+            Title = "";
+            Location = "";
+            Date = "";
+            Time = "";
+            Price = 0.0;
+        }
+
+        // Constructor with parameters
+        public Concert(string title, string location, string date, string time, double price)
+        {
+            Title = title;
+            Location = location;
+            Date = date;
+            Time = time;
+            Price = price;
+        }
+
+        // Method to get all concert information as text
+        public string GetConcertInfo()
+        {
+            return string.Format("Title: {0} | Location: {1} | Date: {2} | Time: {3} | Price: {4} eur", Title, Location, Date, Time, Price); 
+        }
+
+        // Operator to compare two Concert objects
+        public static bool operator ==(Concert c1, Concert c2)
+        {
+            return c1.Equals(c2);
+        }
+
+        // Operator to compare two Concert objects
+        public static bool operator !=(Concert c1, Concert c2)
+        {
+            return !c1.Equals(c2);
+        }
+
+        // Operator to compare if Concert objects is larger
+        public static bool operator < (Concert c1, Concert c2)
+        {
+            return c1.Price < c2.Price;
+        }
+
+        // Operator to compare if Concert object is smaller
+        public static bool operator > (Concert c1, Concert c2)
+        {
+            return c1.Price > c2.Price;
+        }
+
+        // Operator to increase the concert price by 5 units
+        public static Concert operator ++(Concert c)
+        {
+            c.Price += 5;
+            return c;
+        }
+
+        // Operator to decrease the concert price by 5 units
+        public static Concert operator --(Concert c)
+        {
+            c.Price -= 5;
+            return c;
+        }
+    }
+}
---
+++ Assignment_3/Assignment_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an ArrayList and add Concert objects
+            ArrayList concerts = new ArrayList();
+            concerts.Add(new Concert("Rockfest", "Hyvinkää", "08/06/2023", "20:00", 80.00));
+            concerts.Add(new Concert("Classical Concert", "Helsinki", "01/04/2023", "19:00", 40.00));
+            concerts.Add(new Concert("Pori Jazz", "Pori", "07/07/2023", "18:00", 65.50));
+            concerts.Add(new Concert("Ruisrock", "Turku", "07/07/2023", "17:00", 99.99));
+            concerts.Add(new Concert("Weekend festival", "Espoo", "04/08/2023", "16:00", 80.00));
+
+            // Sort the Concert objects in ascending order based on their price
+            concerts.Sort(new ConcertComparer());
+
+            // Print out the Concert objects information
+            Console.WriteLine("Concerts Information");
+            Console.WriteLine("____________________");
+            foreach (Concert c in concerts)
+            {
+                Console.WriteLine(c.GetConcertInfo());
+            }
+
+        }
+    }
+
+    // Comparer class to compare Concert objects
+    public class ConcertComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            Concert c1 = (Concert)x;
+            Concert c2 = (Concert)y;
+            return c1.Price.CompareTo(c2.Price);
+        }
+    }
+}
